ec7e0cd097db0de52b3d751a1ad1bb1c3b86f75c
ehrifqi <torrosel@gmail.com>
2020-09-11 06:47:21 +0700

    Created faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,12 +1,15 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\Model\Product;
 use Faker\Generator as Faker;
 
-$factory->define(Product::class, function (Faker $faker) {
+/* @var Illuminate\Database\Eloquent\Factory $factory */
+
+$factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->word,
+        'detail' => $faker->paragraph,
+        'price' => $faker->numberBetween(100, 1000),
+        'stock' => $faker->randomDigit,
+        'discount' => $faker->numberBetween(2, 30)
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,12 +5,14 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        factory(App\Model\Product::class, 50)->create();
+        factory(App\Model\Review::class, 300)->create();
     }
 }
